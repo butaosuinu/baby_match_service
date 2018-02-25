51c6f39ea1dd7fdc5e147a3f90585c2a3861ff0e
butaosuinu <buta.osuinu@gmail.com>
2018-02-25 20:14:56 +0900

    fix

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,6 +63,7 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'is_babysitter' => $data['is_babysitter'],
         ]);
     }
 }
